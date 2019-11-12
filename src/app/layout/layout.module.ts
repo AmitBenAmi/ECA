d2d91543218d79cee063c3c099bd3d0aded5b7a0
Itay Gabbay <itaygabb@gmail.com>
2019-11-12 15:24:07 +0200

    Adding navigation comp

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   declarations: [
     LayoutComponent,
--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  providers:[MatSidenav]
 })
+
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sidenav: MatSidenav) { 
+    
+  }
 
+  public toggle() {
+    console.log('asdsadsad')
+    this.sidenav.toggle()
+  }
   ngOnInit() {
   }
 
