ea396449b6b517a6169cfba3346192dd2740a523
AmitBenAmi <benamiamit0@gmail.com>
2019-11-13 16:06:26 +0200

    Filling the Http service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { CoreComponent } from './core.component';
 import { AppRoutingModule } from '../app-routing.module';
 
@@ -12,9 +13,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 
+import { HttpService } from './services/http/http.service';
+
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     MatToolbarModule,
     MatIconModule,
     MatMenuModule,
@@ -27,6 +31,9 @@ import { MatListModule } from '@angular/material/list';
   declarations: [CoreComponent, NavComponent],
   exports: [
     NavComponent
+  ],
+  providers: [
+    HttpService
   ]
 })
 export class CoreModule { }
--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class HttpService {
 
-constructor() { }
+    constructor(private httpClient: HttpClient,
+        private loggerService: LoggerService) { }
 
+    async get(url: string, options?: HttpRequestOptions) {
+        try {
+            return await this.httpClient.get(url, options).toPromise();
+        } catch (exception) {
+            this.loggerService.error("Error in HTTP Get.", exception)
+        }
+    }
+
+    async post(url: string, body?: any, options?: HttpRequestOptions) {
+        try{
+            return await this.httpClient.post(url, body, options).toPromise();
+        } catch (exception) {
+            this.loggerService.error("Error in HTTP Post.", exception)
+        }
+    }
+
+    async put(url: string, body?: any, options?: HttpRequestOptions) {
+        try {
+            return await this.httpClient.put(url, body, options).toPromise();
+        } catch (exception) {
+            this.loggerService.error("Error in HTTP Put.", exception)
+        }
+    }
+
+    async delete(url: string, options?: HttpRequestOptions) {
+        try {
+            return await this.httpClient.delete(url, options).toPromise();
+        } catch (exception) {
+            this.loggerService.error("Error in HTTP Delete.", exception)
+        }
+    }
+}
+
+export class HttpRequestOptions {
+    constructor(
+        public headers?: { [name: string]: string | string[] }, 
+        public params?: { [name: string]: string | string[] }, 
+        public reportProgress?: boolean, 
+        public responseType?: 'json', 
+        public withCredentials?: boolean) {  }
 }
