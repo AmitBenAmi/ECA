fbb73b7c7b9244cd6c3057552597f1afe86a37aa
AmitBenAmi <benamiamit0@gmail.com>
2019-11-19 16:03:13 +0200

    Adding the events service

---
+++ src/app/services/events/events.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { DataService } from '../../core/services/data/data.service';
+import { HttpService } from '../../core/services/http/http.service';
+import { LoggerService } from '../../core/services/logger/logger.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService extends DataService {
+
+  constructor(httpService: HttpService, loggerService: LoggerService) {
+    super(httpService, loggerService);
+  }
+
+  getData() {
+    return [{
+      name: 'Amit',
+      lastName: 'Ben Ami'
+    }];
+  }
+}
